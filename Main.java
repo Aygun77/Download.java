65074e5a56ca75714468c9f7d8c2a80213d9af20
Aygun <aygun.2304@gmail.com>
2022-03-16 15:27:38 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,56 @@
+
+//import java.util.*;
+import java.lang.Math;
+class Main {
+  
+  
+  
+  public static void main(String[] args) {
+
+    int randomNumber;
+
+    int minimum = 1;
+    int maximum = 10 - minimum;
+
+    randomNumber = (int)((Math.random()*maximum) + minimum);
+
+    System.out.println("Account Number : " + randomNumber);
+
+//
+//
+//
+//Scanner boom=new Scanner(System.in);
+//
+//System.out.println("name:");
+//      String name= boom.next();
+//System.out.println("last name:");
+//      String last =boom.next();
+//System.out.println("age:");
+//      byte age= boom.nextByte();
+//System.out.println("SSN:");
+//      long SSN= boom.nextLong();
+//System.out.println("Phone:");
+//      long phone=boom.nextLong();
+//System.out.println("Income:");
+//      long income=boom.nextLong();
+//System.out.println("married:");
+//      boolean married=boom.nextBoolean();
+//  boom.nextLine();
+//System.out.println("adress:");
+//      String adress= boom.nextLine();
+//
+//System.out.println("Thanks, here's the info that you entered:");
+//System.out.println("Name: " + name);
+//System.out.println("Last Name:" + last);
+//System.out.println("age:" +age);
+//System.out.println("SSN:"+SSN);
+//System.out.println("Phone:"+phone);
+//System.out.println("Income:" +income);
+//System.out.println("married:"+ married);
+//System.out.println("adress:"+ adress);
+//
+//
+
+
+  }
+}
